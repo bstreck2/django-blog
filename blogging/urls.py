62aa0df226b64cfa209c020632c88204de4c641d
bstre <bstreck.2@gmail.com>
2024-01-13 15:15:39 -0800

    List view

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blogging.views import stub_view
+from blogging.views import stub_view, list_view_v2
 
 urlpatterns = [
-    path('', stub_view, name="blog_index"),
+    path('', list_view_v2, name="blog_index"),
+    path('posts/<int:post_id>/', stub_view, name="blog_detail"),
 ]
